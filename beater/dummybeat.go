bc7bd0eda6297bf15fdd72ea752dce07e40c610b
Akshaytermin <47350950+Akshaytermin@users.noreply.github.com>
2019-10-23 09:13:30 +0530

    Add files via upload

---
+++ beater/dummybeat.go
@@ -0,0 +1,88 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package beater
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/elastic/beats/libbeat/beat"
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/logp"
+
+	"github.com/Akshaytermin/dummybeat/config"
+)
+
+// Dummybeat configuration.
+type Dummybeat struct {
+	done   chan struct{}
+	config config.Config
+	client beat.Client
+}
+
+// New creates an instance of dummybeat.
+func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
+	c := config.DefaultConfig
+	if err := cfg.Unpack(&c); err != nil {
+		return nil, fmt.Errorf("Error reading config file: %v", err)
+	}
+
+	bt := &Dummybeat{
+		done:   make(chan struct{}),
+		config: c,
+	}
+	return bt, nil
+}
+
+// Run starts dummybeat.
+func (bt *Dummybeat) Run(b *beat.Beat) error {
+	logp.Info("dummybeat is running! Hit CTRL-C to stop it.")
+
+	var err error
+	bt.client, err = b.Publisher.Connect()
+	if err != nil {
+		return err
+	}
+
+	ticker := time.NewTicker(bt.config.Period)
+	counter := 1
+	for {
+		select {
+		case <-bt.done:
+			return nil
+		case <-ticker.C:
+		}
+
+		event := beat.Event{
+			Timestamp: time.Now(),
+			Fields: common.MapStr{
+				"type":    b.Info.Name,
+				"counter": counter,
+			},
+		}
+		bt.client.Publish(event)
+		logp.Info("Event sent")
+		counter++
+	}
+}
+
+// Stop stops dummybeat.
+func (bt *Dummybeat) Stop() {
+	bt.client.Close()
+	close(bt.done)
+}
